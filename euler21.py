db99aee5af4bfa80821f640aef0a78e12bb23943
Simon <xxx>
2012-10-21 12:43:05 +0200

    Added euler21.py, with names.txt (working)

---
+++ euler21.py
@@ -0,0 +1,21 @@
+def euler21():
+    total_sum = 0
+    name_file = open("names.txt", "r")
+    name_list = sorted(name_file.read().replace('"',"").split(","))
+    for j, name in enumerate(name_list):
+        name_sum = 0
+        for letter in name:
+            # Sum the letters in the name (only big letters)
+            name_sum += ord(letter)-64
+        # Add the position in the list to the sum
+        total_sum = total_sum + (name_sum * (j + 1))
+        print name + "   " + str(name_sum) + "  " + str(total_sum)
+
+    print total_sum
+        
+#    print name_list
+def main():
+    euler21()
+
+if __name__ == "__main__":
+    main()
