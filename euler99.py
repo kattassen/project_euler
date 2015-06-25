52766d11bf84c3c91b22df85743c4a274aa1ce5d
Simon Furborg <simfur@marvell.com>
2015-06-25 10:02:22 +0200

    Fixed fast solution for 99

--- euler99.py
+++ euler99.py
@@ -1,9 +1,10 @@
+import math
 old_num = 0
 best_line = 0
 f = open("p099_base_exp.txt")
 for line in f:
 	print line
-	x = int(line.split(',')[0])**int(line.split(',')[1])
+	x = int(line.split(',')[1]) * math.log(int(line.split(',')[0]))
 	if x > old_num:
 		print "best so far!"
 		old_num = x
